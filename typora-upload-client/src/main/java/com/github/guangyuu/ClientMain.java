19b7178ad95305a2d90ec0cfbf14240ae84ac162
guangyu <779112613@qq.com>
2022-11-03 23:59:39 +0800

    配置维护

---
+++ typora-upload-client/src/main/java/com/github/guangyuu/ClientMain.java
@@ -0,0 +1,12 @@
+package com.github.guangyuu;
+
+/**
+ * @Author Guangyu
+ * @Date 2022/11/3
+ * @Description 主程序
+ **/
+public class ClientMain {
+    public static void main(String[] args) {
+
+    }
+}
