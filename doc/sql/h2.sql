bf84914e633910756cb7547947af261549ccca2e
guangyu <779112613@qq.com>
2022-11-04 16:39:01 +0800

    新增表

--- doc/sql/h2.sql
+++ doc/sql/h2.sql
@@ -6,4 +6,16 @@ CREATE TABLE tb_sys_user
     `del_flag`     TINYINT ( 1 ) UNSIGNED NOT NULL DEFAULT '0',
     `username`     VARCHAR(100) NOT NULL,
     `access_token` VARCHAR(255) NOT NULL
-)
+);
+
+CREATE TABLE `tb_sys_setting`
+(
+    `id`          BIGINT ( 20 ) PRIMARY KEY AUTO_INCREMENT,
+    `create_time` datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `del_flag`    TINYINT ( 1 ) UNSIGNED NOT NULL DEFAULT '0',
+    `name`        VARCHAR(50) NOT NULL COMMENT '名称',
+    `value`       VARCHAR(50) NOT NULL COMMENT '值',
+    `desc`        VARCHAR(50)          DEFAULT NULL COMMENT '描述'
+);
+INSERT INTO tb_sys_setting(`name`, `value`, `desc`) values ('jwt.secret.key','c^3B1!4Y#8C$Dxy#','jwt签名密钥')
