89ae5f84c8834107f95450393beeeaa3c58ac969
guangyu <779112613@qq.com>
2022-11-04 22:12:27 +0800

    元数据类

---
+++ typora-upload-service/src/main/java/com/github/guangyuu/common/pojo/po/FileMetadata.java
@@ -0,0 +1,36 @@
+package com.github.guangyuu.common.pojo.po;
+
+import lombok.Data;
+
+/**
+ * @Author Guangyu
+ * @Date 2022/9/16
+ * @Description 文件元数据
+ **/
+@Data
+public class FileMetadata {
+    /**
+     * 文件签名
+     */
+    private String sign;
+    /**
+     * 原文件名
+     */
+    private String originalName;
+    /**
+     * 文件存储名称
+     */
+    private String storeName;
+    /**
+     * 存储时间
+     */
+    private Long storeTime;
+    /**
+     * 文件标识
+     */
+    private String fileUid;
+    /**
+     * 存储路径
+     */
+    private String path;
+}
